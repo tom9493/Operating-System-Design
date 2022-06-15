de489f138fddf6090dcfa826fdde2b25c978df95
tom9493 <tom9493@gmail.com>
2022-06-15 14:33:39 -0600

    Define and delete seem to work

--- os345fat.c
+++ os345fat.c
@@ -81,6 +81,30 @@ extern int curTask;							// current task #
 
 int numOpenFiles = 0;
 
+void getFilename(char* filename, char* name, char* extension)
+{
+	int i;
+	int flag = 0;
+	int length = strlen(filename);
+	for (i = 0; i < length; i++) filename[i] = toupper(filename[i]);
+	for (i = 0; i < length; i++)
+	{
+		if (filename[i] == '.') break;
+	}
+
+	memcpy(name, filename, i);
+	memcpy(&name[i], "         ", 8 - i);
+	if (i != length)
+	{
+		int extensionLen = length - i - 1;
+		memcpy(extension, &filename[i + 1], extensionLen);
+		memcpy(&extension[extensionLen], "   ", 3 - extensionLen);
+	}
+	else
+	{
+		memcpy(extension, "   ", 3);
+	}
+}
 
 // ***********************************************************************
 // ***********************************************************************
@@ -90,11 +114,11 @@ int numOpenFiles = 0;
 //
 int fmsCloseFile(int fileDescriptor)
 {
-	int error;
+	int error, root;
 	char buffer[BYTES_PER_SECTOR];
-	if (!diskMounted) return ERR72;		// Check if disk is mounted
-	if ((fileDescriptor >= NFILES) || (fileDescriptor < 0)) return ERR52;
-	if (OFTable[fileDescriptor].name[0] == 0) return ERR63;
+	if (!diskMounted) return ERR72;											// Check if disk is mounted
+	if ((fileDescriptor >= NFILES) || (fileDescriptor < 0)) return ERR52;	// Check invalid file descriptor
+	if (OFTable[fileDescriptor].name[0] == 0) return ERR63;					// 
 
 	// C_2_S changes cluster number to sector number
 
@@ -102,40 +126,55 @@ int fmsCloseFile(int fileDescriptor)
 	{
 		// save back to disk sector
 		int sectorNumber = C_2_S(OFTable[fileDescriptor].currentCluster);
-		if ((error = fmsWriteSector(&OFTable[fileDescriptor].buffer, sectorNumber)) < 0) return error;
+		if (error = fmsWriteSector(&OFTable[fileDescriptor].buffer, sectorNumber)) return error;
 
-		// Get sector data fram the directory cluster sector
-		sectorNumber = C_2_S(OFTable[fileDescriptor].directoryCluster);
-		if ((error = fmsReadSector(&buffer, sectorNumber)) < 0) return error;		// Reading a sector from memory into the buffer
-
-		DirEntry dirEntry;
-
-		// Update dirEntry corresponding to this
-		if ((error = fmsGetDirEntry(OFTable[fileDescriptor].name, &dirEntry)) < 0) return error;
-
-		// Update date time 
-		setDirTimeDate(&dirEntry);
-
-		// Update file size
-		dirEntry.fileSize = OFTable[fileDescriptor].fileSize;
-		// Get dir entry address and save dir entry
-		DirEntry tempDirEntry;
-		for (int i = 0; i < ENTRIES_PER_SECTOR; ++i)
+		if (OFTable[fileDescriptor].directoryCluster == 0)
 		{
-			memcpy(&tempDirEntry, &buffer[i * sizeof(DirEntry)], sizeof(DirEntry));		// Go through each entry per sector and copy the buffer of that directory and multiply it by the directory entry a
+			sectorNumber = BEG_DATA_SECTOR;
+			root = 1;
+		}
+		else
+		{
+			sectorNumber = C_2_S(OFTable[fileDescriptor].directoryCluster);
+			root = 0;
+		}
+		
+		if (error = fmsReadSector(buffer, sectorNumber)) return error;
 
-			if (tempDirEntry.name[0] == 0) return ERR67;				// EOD, why does it return?
-			if (tempDirEntry.name[0] == 0xe5);
-			else if (tempDirEntry.attributes == LONGNAME);
-			else if (fmsMask(OFTable[fileDescriptor].name, tempDirEntry.name, tempDirEntry.extension))	// Matches name. Makes sure OFTable thing matches the name and extension?
+		char* bufIndex = NULL;
+		char* tempBuf = buffer;
+		while (1)
+		{
+			for (int i = 0; i < ENTRIES_PER_SECTOR; i++)
 			{
-				memcpy(&buffer[i * sizeof(DirEntry)], &dirEntry, sizeof(DirEntry));		// This is writing the OFT entry back to the buffer, which will write back into memory (after you found the matching name and extension)
+				if (strncmp(tempBuf, OFTable[fileDescriptor].name, 11))
+				{
+					tempBuf += sizeof(DirEntry);
+				}
+				else
+				{
+					bufIndex = tempBuf; 
+					break;
+				}
+				if (i == ENTRIES_PER_SECTOR - 1) bufIndex == NULL;
+			}
+			if (bufIndex == NULL)
+			{
+				if (root) sectorNumber++;
+				else sectorNumber = C_2_S(getFatEntry(S_2_C(sectorNumber), FAT1));
+				if (error = fmsReadSector(buffer, sectorNumber)) return error;
+			}
+			else
+			{
+				((DirEntry*)bufIndex)->fileSize = OFTable[fileDescriptor].fileSize;
+				((DirEntry*)bufIndex)->startCluster = OFTable[fileDescriptor].startCluster;
+				setDirTimeDate(((DirEntry*)bufIndex));
+				if (error = fmsWriteSector(buffer, sectorNumber)) return error;
 				break;
 			}
 		}
 
-		// Write back to disk sector
-		if ((error = fmsWriteSector(&buffer, sectorNumber)) < 0) return error;			// Writes the changed buffer back to memory
+		
 	}
 	// Set first bit to zero		
 	numOpenFiles--;									// With the file closing, there are 1 fewer files open
@@ -163,11 +202,106 @@ int fmsCloseFile(int fileDescriptor)
 //
 int fmsDefineFile(char* fileName, int attribute)
 {
-	if (!diskMounted) return ERR72;		// Check if disk is mounted
-	// ?? add code here
-	printf("\nfmsDefineFile Not Implemented");
+	int error, i, sectorNum, clusterNum, nextCluster, root;
+	char buffer[BYTES_PER_SECTOR], currentDirBuf[BYTES_PER_SECTOR];
+	DirEntry dirEntry;
 
-	return ERR72;
+	if (!diskMounted) return ERR72;							// Check if disk is mounted
+	if (isValidFileName(fileName) < 1) return ERR50;
+	error = fmsGetDirEntry(fileName, &dirEntry);
+	if (error != ERR61) return ERR60;						// If this doesn't return "file not defined", the file is already defined, which is wrong. Return "file already defined".
+
+	clusterNum = CDIR;
+	if (clusterNum == 0)
+	{
+		sectorNum = 19;
+		root = 1;
+	}
+	else
+	{
+		sectorNum = C_2_S(clusterNum);
+		root = 0;
+	}
+
+	while (1)
+	{
+		if (error = fmsReadSector(currentDirBuf, sectorNum)) return error;
+		for (i = 0; i < ENTRIES_PER_SECTOR; i++)
+		{
+			if (currentDirBuf[i * sizeof(DirEntry)] == 0xe5) continue;
+			if (currentDirBuf[i * sizeof(DirEntry)] == 0) break;
+		}
+		if (i == ENTRIES_PER_SECTOR)
+		{
+			if (root) sectorNum++;
+			else
+			{
+				clusterNum = S_2_C(sectorNum);
+				nextCluster = getFatEntry(clusterNum, FAT1);
+				if (nextCluster == FAT_EOC)
+				{
+					int newCluster = 2;
+					while (getFatEntry(newCluster, FAT1) != 0) newCluster++;
+					setFatEntry(clusterNum, newCluster, FAT1);
+					setFatEntry(newCluster, FAT_EOC, FAT1);
+					memcpy(FAT2, FAT1, 4608);
+					clusterNum = newCluster;
+					sectorNum = C_2_S(clusterNum);
+					// new buf here for errors below?
+					memset(buffer, 0, BYTES_PER_SECTOR);
+					if (error = fmsWriteSector(buffer, sectorNum)) return error;
+				}
+				else sectorNum = C_2_S(nextCluster);
+			}
+		}
+		else break;
+	}
+
+	if (attribute & DIRECTORY)
+	{
+		DirEntry temp;
+
+		int cluster = 2;
+		while (getFatEntry(cluster, FAT1) != 0) cluster++;
+		setFatEntry(cluster, FAT_EOC, FAT1);
+		memcpy(FAT2, FAT1, 4608);
+
+		memset(buffer, 0, BYTES_PER_SECTOR);
+		setDirTimeDate(&temp);
+		temp.attributes = 0x10;
+		temp.fileSize = 0;
+		temp.startCluster = cluster;
+		memcpy(temp.name, ".       ", 8);
+		memcpy(temp.extension, "   ", 3);
+		memcpy(buffer, &temp, sizeof(DirEntry));
+		
+		temp.startCluster = clusterNum;
+		memcpy(temp.name, "..      ", 8);
+		memcpy(temp.extension, "   ", 3);
+		memcpy(&buffer[sizeof(DirEntry)], &temp, sizeof(DirEntry));
+		int sector = C_2_S(cluster);
+		if (error = fmsWriteSector(buffer, sector)) return error;
+		
+		getFilename(fileName, dirEntry.name, dirEntry.extension);
+		dirEntry.attributes = attribute;
+		dirEntry.startCluster = cluster;
+		dirEntry.fileSize = 0;
+		setDirTimeDate(&dirEntry);
+		memcpy(&currentDirBuf[i * sizeof(DirEntry)], &dirEntry, sizeof(DirEntry));
+		if (error = fmsWriteSector(currentDirBuf, sectorNum)) return error;
+	}
+	else
+	{
+		getFilename(fileName, dirEntry.name, dirEntry.extension);
+		dirEntry.attributes = attribute;
+		dirEntry.startCluster = 0;
+		dirEntry.fileSize = 0;
+		setDirTimeDate(&dirEntry);
+		memcpy(&currentDirBuf[i * sizeof(DirEntry)], &dirEntry, sizeof(DirEntry));
+		if (error = fmsWriteSector(currentDirBuf, sectorNum)) return error;
+	}
+
+	return 0;
 } // end fmsDefineFile
 
 
@@ -181,11 +315,99 @@ int fmsDefineFile(char* fileName, int attribute)
 //
 int fmsDeleteFile(char* fileName)
 {
-	if (!diskMounted) return ERR72;		// Check if disk is mounted
-	// ?? add code here
-	printf("\nfmsDeleteFile Not Implemented");
+	DirEntry dirEntry;
+	char buffer[BYTES_PER_SECTOR];
+	int error, i, fileDescriptor, root, dirSector, dirCluster, currCluster, nextCluster, tempCluster;
+	int index = 0;
 
-	return ERR61;
+	if (!diskMounted) return ERR72;															// Check if disk is mounted
+	if (isValidFileName(fileName) < 1) return ERR50;										// Invald file name
+	if ((error = fmsGetNextDirEntry(&index, fileName, &dirEntry, CDIR)) < 0) return error;	// File not defined, or file space full? Probs not
+
+	if (dirEntry.attributes & DIRECTORY)
+	{
+		currCluster = dirEntry.startCluster;
+		if (dirEntry.startCluster == 0)
+		{
+			dirSector = 19;
+			root = 1;
+		}
+		else
+		{
+			root = 0;
+			dirSector = C_2_S(currCluster);
+		}
+		while (1)
+		{
+			if (error = fmsReadSector(buffer, dirSector)) return error;
+			for (i = 0; i < ENTRIES_PER_SECTOR; i++)
+			{
+				if (buffer[i * sizeof(DirEntry)] == 0) break;
+				else if (buffer[i * sizeof(DirEntry)] == 0x2e) continue;
+				else if (buffer[i * sizeof(DirEntry)] == 0xffffffe5) continue;    // Deleted entry
+				else return ERR69;
+			}
+			if (i == ENTRIES_PER_SECTOR)
+			{
+				if (root)
+				{
+					dirSector++;
+					if (dirSector >= BEG_DATA_SECTOR) break;
+				}
+				else
+				{
+					if (currCluster == FAT_EOC) break;
+					currCluster = getFatEntry(S_2_C(dirSector), FAT1);
+					dirSector = C_2_S(currCluster);
+				}
+			}
+			else break;
+		}
+	}
+	
+	// Make sure file is not already open
+	for (int i = 0; i < NFILES; ++i)
+	{
+		if (strncmp(OFTable[i].name, dirEntry.name, 11) == 0) { return ERR62; }			// strncmp returns 0 if same
+	}
+
+	int loop = index / ENTRIES_PER_SECTOR;
+	dirCluster = CDIR;
+	if (dirCluster == 0)
+	{
+		dirSector = 19;
+		while(loop--) dirSector++;
+	}
+	else
+	{
+		while (loop--)
+		{
+			nextCluster = getFatEntry(dirCluster, FAT1);
+			if (nextCluster == FAT_EOC) break;
+			dirCluster = nextCluster;
+		}
+	}
+
+	index = (index % ENTRIES_PER_SECTOR) - 1;
+	dirEntry.name[0] = 0xe5;
+	if (error = fmsReadSector(buffer, dirSector)) return error;
+	memcpy(&buffer[index * sizeof(DirEntry)], &dirEntry, sizeof(DirEntry));
+	if (error = fmsWriteSector(buffer, dirSector)) return error;
+
+	currCluster = 0;
+	nextCluster = dirEntry.startCluster;
+	if (dirEntry.startCluster != 0)
+	{
+		while (1)
+		{
+			currCluster = nextCluster;
+			nextCluster = getFatEntry(currCluster, FAT1);
+			setFatEntry(currCluster, 0, FAT1);
+			if (currCluster == FAT_EOC) break;
+		}
+		memcpy(FAT2, FAT1, 4608);
+	}
+	return 0;
 } // end fmsDeleteFile
 
 
@@ -221,16 +443,15 @@ int fmsOpenFile(char* fileName, int rwMode)
 	if (!diskMounted) return ERR72;														// Check if disk is mounted
 	if (isValidFileName(fileName) < 1) return ERR50;									// Invald file name
 	if (numOpenFiles >= 32) return ERR70;												// Too many files open
-	if ((error = fmsGetDirEntry(fileName, &dirEntry)) < 0) return error;					// File not defined, or file space full? Probs not
+	if ((error = fmsGetDirEntry(fileName, &dirEntry)) < 0) return error;				// File not defined, or file space full? Probs not
 
 	// Make sure file is not already open
 	for (int i = 0; i < NFILES; ++i)
 	{
-		//printf("OFTable[%d].name -- dirEntry.name: %s -- %s\n", i, OFTable[i].name, dirEntry.name);
-		if (strncmp(OFTable[i].name, dirEntry.name, 11) == 0) { return ERR62; }				// strncmp returns 0 if same
+		if (strncmp(OFTable[i].name, dirEntry.name, 11) == 0) { return ERR62; }			// strncmp returns 0 if same
 	}
-
-	memcpy(newFCB.name, dirEntry.name, 8);
+	
+	memcpy(newFCB.name, dirEntry.name, 8);												// Copying all dirEntry info into the FCB
 	memcpy(newFCB.extension, dirEntry.extension, 3);									// Memcpy so not null terminated
 	newFCB.attributes = dirEntry.attributes;
 	newFCB.directoryCluster = CDIR;
@@ -270,49 +491,50 @@ int fmsOpenFile(char* fileName, int rwMode)
 //
 int fmsReadFile(int fileDescriptor, char* buffer, int nBytes)
 {
-	if (!diskMounted) return ERR72;		// Check if disk is mounted, should be in every function
-	// If file descriptor is in boundaries
-	if ((fileDescriptor >= NFILES) || (fileDescriptor < 0)) { return ERR52; }
+	if (!diskMounted) return ERR72;																// Check if disk is mounted, should be in every function
+	if ((fileDescriptor >= NFILES) || (fileDescriptor < 0)) { return ERR52; }					// If file descriptor is in boundaries
+	
 	int error, nextCluster;
 	FCB* entry;
 	int numBytesRead = 0;
 	unsigned int bytesLeft, bufferIndex;
-	entry = &OFTable[fileDescriptor];							// 
-	if (entry->name[0] == 0) return ERR63;						// File not open
-	if ((entry->mode == 1) || (entry->mode == 2)) return ERR85; // Incorrect mode
+	entry = &OFTable[fileDescriptor];															// Gets entry of OFTable 
+	
+	if (entry->name[0] == 0) return ERR63;														// File not open
+	if ((entry->mode == 1) || (entry->mode == 2)) return ERR85;									// Incorrect mode
 	while (nBytes > 0)
 	{
-		if (entry->fileSize == entry->fileIndex) return (numBytesRead ? numBytesRead : ERR66); // Read fewer than requested or none (EOF)
-		bufferIndex = entry->fileIndex % BYTES_PER_SECTOR;
-		if ((bufferIndex == 0) && (entry->fileIndex || !entry->currentCluster))
+		if (entry->fileSize == entry->fileIndex) return (numBytesRead ? numBytesRead : ERR66);	// Read fewer than requested or none (EOF)
+		bufferIndex = entry->fileIndex % BYTES_PER_SECTOR;										// Buffer index is the number of bytes the file is reading into the current sector its in
+		if ((bufferIndex == 0) && (entry->fileIndex || !entry->currentCluster))					// If we are switching to next cluster. Sets nextCluster
 		{
-			if (entry->currentCluster == 0)
+			if (entry->currentCluster == 0)														// If we are at the start of the file
 			{
-				if (entry->startCluster == 0) return ERR66;
-				nextCluster = entry->startCluster;
-				entry->fileIndex = 0;
+				if (entry->startCluster == 0) return ERR66;										// startCluster gets set to 0 if this is an empty file. Define file will set this to right cluster
+				nextCluster = entry->startCluster;												// If not empty file, set nextCluster to first cluster in file
+				entry->fileIndex = 0;															// File index is also set to zero
 			}
 			else
 			{
-				nextCluster = getFatEntry(entry->currentCluster, FAT1);
-				if (nextCluster == FAT_EOC) return numBytesRead;
+				nextCluster = getFatEntry(entry->currentCluster, FAT1);							// If not at start of file, use fat table to get next cluster
+				if (nextCluster == FAT_EOC) return numBytesRead;								// return numBytes read if next cluster is marked as end of chain
 			}
-			if (entry->flags & BUFFER_ALTERED)
+			if (entry->flags & BUFFER_ALTERED)													// Don't read if a write needs to be written back. Current cluster of file is only one that is in OFT?
 			{
-				if ((error = fmsWriteSector(entry->buffer, C_2_S(entry->currentCluster)))) return error;
-				entry->flags &= ~BUFFER_ALTERED;
+				if ((error = fmsWriteSector(entry->buffer, C_2_S(entry->currentCluster)))) return error;	// Writes back to memory
+				entry->flags &= ~BUFFER_ALTERED;															// Takes away the BUFFER_ALTERED flag
 			}
-			entry->currentCluster = nextCluster;
-			if ((error = fmsReadSector(entry->buffer, C_2_S(entry->currentCluster)))) return error;
+			entry->currentCluster = nextCluster;															// currentCluster in entry is set to nextCluster we are moving to
+			if ((error = fmsReadSector(entry->buffer, C_2_S(entry->currentCluster)))) return error;			// Reads sector from memory and writes it into the entry buffer
 		}
-		bytesLeft = BYTES_PER_SECTOR - bufferIndex;
-		if (bytesLeft > nBytes) bytesLeft = nBytes;
-		if (bytesLeft > (entry->fileSize - entry->fileIndex)) bytesLeft = entry->fileSize - entry->fileIndex;
-		memcpy(buffer, &entry->buffer[bufferIndex], bytesLeft);
-		entry->fileIndex += bytesLeft;
-		numBytesRead += bytesLeft;
-		buffer += bytesLeft;
-		nBytes -= bytesLeft;
+		bytesLeft = BYTES_PER_SECTOR - bufferIndex;												// Bytes left we can read from this cluster/sector
+		if (bytesLeft > nBytes) bytesLeft = nBytes;												// Only read the number of bytes requested instead of the whole sector
+		if (bytesLeft > (entry->fileSize - entry->fileIndex)) bytesLeft = entry->fileSize - entry->fileIndex;// Only read the amount of bytes left in the file instead of past that
+		memcpy(buffer, &entry->buffer[bufferIndex], bytesLeft);									// Copy bytes from entry starting at buffer index, bytesLeft amount into the buffer given
+		entry->fileIndex += bytesLeft;															// Set fileIndex up to where we stopped reading
+		numBytesRead += bytesLeft;																// Give the number of bytes we were actually able to read regardless of request
+		buffer += bytesLeft;																	// Increment buffer so next read appends to the end 
+		nBytes -= bytesLeft;																	// "bytesLeft" fewer bytes to read, so decrease nBytes
 	}
 	return numBytesRead;
 } // end fmsReadFile
